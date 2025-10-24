35edd847828f14ac211894fbf0a1f082ee74caa7
strumgewehr <sethi.garv2006@gmail.com>
2025-10-24 17:59:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,42 +4,39 @@ import path from "path";
 import { fileURLToPath } from "url";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname: string = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(async () => {
-  const plugins = [
+  const plugins: any[] = [
     react(),
     runtimeErrorOverlay(),
   ];
 
-  if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
+  if (process.env.NODE_ENV !== "production" && process.env.REPL_ID) {
     const cartographer = await import("@replit/vite-plugin-cartographer").then(m => m.cartographer());
     const devBanner = await import("@replit/vite-plugin-dev-banner").then(m => m.devBanner());
     plugins.push(cartographer, devBanner);
   }
 
-return {
-  plugins,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "client", "src"),
+        "@shared": path.resolve(__dirname, "shared"),
+        "@assets": path.resolve(__dirname, "attached_assets"),
+      },
     },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: path.resolve(__dirname, "server/public"), // instead of dist/public
-    emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"], // deny hidden files only
+    root: path.resolve(__dirname, "client"),
+    build: {
+      outDir: path.resolve(__dirname, "server/public"),
+      emptyOutDir: true,
     },
-  },
-};
-      
-    
-
-
+    server: {
+      fs: {
+        strict: true,
+        deny: ["**/.*"], // deny hidden files only
+      },
+    },
+  };
+});
