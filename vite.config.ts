8c93cdd74fb6a47137252a5f7dc48ff7c74016d3
strumgewehr <sethi.garv2006@gmail.com>
2025-10-24 17:54:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,25 +18,27 @@ export default defineConfig(async () => {
     plugins.push(cartographer, devBanner);
   }
 
-  return {
-    plugins,
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "client", "src"),
-        "@shared": path.resolve(__dirname, "shared"),
-        "@assets": path.resolve(__dirname, "attached_assets"),
-      },
+return {
+  plugins,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
-    root: path.resolve(__dirname, "client"),
-   build: {
-      outDir: path.resolve(__dirname, "server/public"), // instead of dist/public
-      emptyOutDir: true,
-    }
+  },
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "server/public"), // instead of dist/public
+    emptyOutDir: true,
+  },
+  server: {
+    fs: {
+      strict: true,
+      deny: ["**/.*"], // deny hidden files only
     },
-    server; {
-      fs: {
-        strict: true,
-        deny; ["**/.*"],
+  },
+};
       
     
 
